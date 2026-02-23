abbde9968d5be8d73562a9b2c47f65050823edf0
sbhong <skylork7@gmail.com>
2026-02-23 13:27:43 +0900

    feat: 게시판 기능 수정 완료

--- dockerNginx/frontend-docker/src/api/boardApi.js
+++ dockerNginx/frontend-docker/src/api/boardApi.js
@@ -1,5 +1,6 @@
 export async function getBoardList() {
     const response = await fetch("/api/board");
+
     if (response.ok) {
         const rtnJson = response.json();
         console.log("Board API response", rtnJson);
